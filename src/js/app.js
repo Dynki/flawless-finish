fe19c9d86ef559d4688e1970a7d7e2d436e87827
Dean Selvey <deanselvey@gmail.com>
2019-02-26 20:14:34 +0000

    Asthetic services

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,20 @@
 import '../styles/main.scss';
+import * as $ from 'jquery';
 
+$("#btn-services").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#services-section").offset().top},
+        'slow');
+});
+
+$("#sh-services-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#services-section").offset().top},
+        'slow');
+});
+
+$("#mp-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#sm-section").offset().top},
+        'slow');
+});
